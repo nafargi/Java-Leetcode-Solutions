43569cfc6340635236f1ded5d40132aa8212700d
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-09 12:15:12 -0700

    Create Maximum Number of Words Found in Sentences.java

---
+++ Maximum Number of Words Found in Sentences.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<sentences.length; i++){
+            String sentence=sentences[i];
+            String[] words=sentence.split(" ");
+            max=Math.max(max,words.length);   
+        }
+        return max;
+    }
+}
