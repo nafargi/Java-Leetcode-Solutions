457c75d2041ce93d0eddb33c27aa3feb1f995c51
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-10 01:54:11 -0700

    Create  Left and Right Sum Differences.java

---
+++ Left and Right Sum Differences.java
@@ -0,0 +1,22 @@
+//this code Beats 98.81% of users with Java
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int[] left= new int[nums.length];
+        int[] right= new int[nums.length];
+        left[0]=0;
+        right[nums.length-1]=0;
+      
+        for(int i =0; i<nums.length-1; ++i){
+           left[i+1]=nums[i] + left[i];
+        }
+        for(int i =nums.length -2 ; i >= 0 ; i--) {
+            right[i] = right[i+1] + nums[i+1];  
+        }
+
+        for(int i=0;i<nums.length; ++i){
+            nums[i]=Math.abs(right[i]-left[i]);
+        }
+     return nums;
+    }
+}
