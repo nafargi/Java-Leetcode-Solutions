9c7f2a8d22d96e0f6bcd75ebb86dbd74dbf2d49c
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-10 02:21:44 -0700

    Create Check If Two String Arrays are Equivalent.java

---
+++ Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,14 @@
+//this code Beats 66.99% of users with Java
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+       String stringWord1="";
+       String stringWord2="";
+       for(String word: word1){
+          stringWord1+=word;
+       }
+       for(String word: word2){
+          stringWord2+=word;
+       }
+    return stringWord1.equals(stringWord2);
+    }
+}
