4e26467ffaf834d29f36dc72a7418503426d004d
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-10 00:15:52 -0700

    Create Sum of Values at Indices With K Set Bits.java

---
+++ Sum of Values at Indices With K Set Bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+    int sum=0;
+    for(int i=0; i< nums.size(); i++){
+        int num= nums.get(i);
+        String binaryString = Integer.toBinaryString(i);
+        char[] binaryList= binaryString.toCharArray();
+          
+        int counter=0;
+        for(int j=0; j<binaryList.length; j++){
+            if(binaryList[j] == '1')
+            counter++;
+          } 
+           if(k == counter){
+            sum+=num;
+            }
+       }
+      return sum; 
+    }
+}
