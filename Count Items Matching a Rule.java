cd543e67ada7937b23072ae6ad639d855c6073ca
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-12 15:33:28 -0700

    Create Count Items Matching a Rule.java

---
+++ Count Items Matching a Rule.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int counter=0;
+        for(List<String> item :items){
+            if(ruleKey.equals("type") && ruleValue.equals(item.get(0))){
+              counter++;
+            }
+            else if(ruleKey.equals("color") && ruleValue.equals(item.get(1))){
+              counter++;
+            }
+            else if(ruleKey.equals("name") && ruleValue.equals(item.get(2))){
+              counter++;
+            }  
+        }
+        return counter;
+    }
+}
