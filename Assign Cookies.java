04c158567ed18fbad20a81e9d97252c972006bfa
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-06-12 02:55:40 +0300

    Update Assign Cookies.java

--- Assign Cookies.java
+++ Assign Cookies.java
@@ -1,7 +1,7 @@
 class Solution {
   public int findContentChildren(int[] g, int[] s) {
     Arrays.sort(g);
-    Arrays.sort(s);
+    Arrays.sort(s); rrrrrrrrr
     int i = 0;
     for (int j = 0; i < g.length && j < s.length; ++j)
       if (g[i] <= s[j])
