246158fdadb1d47f355ff309e3e7b94cc5fe2271
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-06-14 02:07:44 +0300

    Update Check If Two String Arrays are Equivalent.java

--- Check If Two String Arrays are Equivalent.java
+++ Check If Two String Arrays are Equivalent.java
@@ -6,7 +6,7 @@ class Solution {
        for(String word: word1){
           stringWord1+=word;
        }
-       for(String word: word2){
+       for(String word: word2){oooo
           stringWord2+=word;
        }
     return stringWord1.equals(stringWord2);
